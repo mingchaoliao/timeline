7ca9d2d8ad9f6f8a2b296d7115e9996736583bd9
Mingchao Liao <liaom@miamioh.edu>
2019-03-05 16:50:40 -0500

    fix image issue

--- web/src/app/modules/core/components/event-card/event-card.component.ts
+++ web/src/app/modules/core/components/event-card/event-card.component.ts
@@ -1,12 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  ViewEncapsulation
-} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import {CommonService} from '../../shared/services/common.service';
 import {UserService} from '../../shared/services/user.service';
@@ -15,9 +7,6 @@ import {Url} from '../../shared/classes/url';
 import {EventService} from '../../shared/services/event.service';
 import {Notification} from '../../shared/models/notification';
 import {NotificationEmitter} from '../../shared/events/notificationEmitter';
-import * as moment from 'moment';
-import {EventDate} from '../../shared/models/event';
-import {Image} from '../../shared/models/image';
 import {LanguageEmitter} from "../../shared/events/languageEmitter";
 import {Language} from "../../shared/models/language";
 
@@ -37,18 +26,18 @@ export class EventCardComponent implements OnInit {
   private _language: Language;
 
   constructor(
-    public sanitizer: DomSanitizer,
-    public common: CommonService,
-    private cdr: ChangeDetectorRef,
-    private eventService: EventService) {
+      public sanitizer: DomSanitizer,
+      public common: CommonService,
+      private cdr: ChangeDetectorRef,
+      private eventService: EventService) {
 
   }
 
-  getImageUrl(image: Image): string {
-    if (image && image.eventId) {
-      return Url.getImage(image.path);
-    } else {
+  getImageUrl(image): string {
+    if (image['id']) {
       return Url.getTempImage(image.path);
+    } else {
+      return Url.getImage(image);
     }
   }
 
@@ -75,15 +64,15 @@ export class EventCardComponent implements OnInit {
 
   public onDelete(id: number) {
     this.eventService.deleteById(id).subscribe(
-      success => {
-        this.isDeleted = true;
-        NotificationEmitter.emit(Notification.success('Event delete successfully.'));
-        this.cdr.detectChanges();
-      },
-      error => {
-        NotificationEmitter.emit(Notification.error(error.error.message));
-        this.cdr.detectChanges();
-      }
+        success => {
+          this.isDeleted = true;
+          NotificationEmitter.emit(Notification.success('Event delete successfully.'));
+          this.cdr.detectChanges();
+        },
+        error => {
+          NotificationEmitter.emit(Notification.error(error.error.message));
+          this.cdr.detectChanges();
+        }
     );
   }
 }
